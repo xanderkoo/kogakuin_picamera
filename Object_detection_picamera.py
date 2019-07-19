4268eda742919dfb5ddd0c4303ea458fdba3fed3
xanderkoo <xanderkoo@gmail.com>
2019-07-19 09:52:33 +0800

    できたと思いますw

--- Object_detection_picamera.py
+++ Object_detection_picamera.py
@@ -159,47 +159,6 @@ camera.framerate = 10
 rawCapture = PiRGBArray(camera, size=(IM_WIDTH,IM_HEIGHT))
 rawCapture.truncate(0)
 
-# for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
-#
-#     t1 = cv2.getTickCount()
-#
-#     # Acquire frame and expand frame dimensions to have shape: [1, None, None, 3]
-#     # i.e. a single-column array, where each item in the column has the pixel RGB value
-#     frame = np.copy(frame1.array)
-#     frame.setflags(write=1)
-#     frame_expanded = np.expand_dims(frame, axis=0)
-#
-#     # Perform the actual detection by running the model with the image as input
-#     (boxes, scores, classes, num) = sess.run(
-#         [detection_boxes, detection_scores, detection_classes, num_detections],
-#         feed_dict={image_tensor: frame_expanded})
-#
-#     # Draw the results of the detection (aka 'visulaize the results')
-#     vis_util.visualize_boxes_and_labels_on_image_array(
-#         frame,
-#         np.squeeze(boxes),
-#         np.squeeze(classes).astype(np.int32),
-#         np.squeeze(scores),
-#         category_index,
-#         use_normalized_coordinates=True,
-#         line_thickness=8,
-#         min_score_thresh=0.40)
-#
-#     cv2.putText(frame,"FPS: {0:.2f}".format(frame_rate_calc),(30,50),font,1,(255,255,0),2,cv2.LINE_AA)
-#
-#     # All the results have been drawn on the frame, so it's time to display it.
-#     cv2.imshow('Object detector', frame)
-#
-#     t2 = cv2.getTickCount()
-#     time1 = (t2-t1)/freq
-#     frame_rate_calc = 1/time1
-#
-#     # Press 'q' to quit
-#     if cv2.waitKey(1) == ord('q'):
-#         break
-#
-#     rawCapture.truncate(0)
-
 # # object detection
 # # 物体検出・認識の部分
 try:
@@ -377,107 +336,18 @@ try:
                 break
 
 
+        # find FPS, print into console
         t2 = cv2.getTickCount()
         time1 = (t2-t1)/freq
         frame_rate_calc = 1/time1
+        print("FPS: " + str(frame_rate_calc))
 
         # Press 'q' while in the cv window to quit
         if cv2.waitKey(1) == ord('q'):
             break
 
         rawCapture.truncate(0)
-#
-#
-#         # print('\nNew Frame')
-#         #
-#         # # iterates thru every TensorFlow detected object
-#         # # TensorFlowの推論の結果をみて、TFが検出した物を一つ一つ処理する
-#         # for idx, s in enumerate(scores[0]):
-#         #
-#         #     # if the TF-detected object has a high enough confidence, continue
-#         #     # to see if it corresponds with a LIDAR-detected object
-#         #     # 物体の検出確率が最低限より高かったら、処理を続ける
-#         #     if s > MIN_CONF:
-#         #
-#         #         # debug用のprint
-#         #         # print(str(category_index[int(classes[0][idx])]))
-#         #         # print('confidence: ' + str(scores[0][idx]))
-#         #         # print('bound: ' + str(boxes[0][idx]))
-#         #
-#         #         # iterates through every element in the lidar_input set, i.e.
-#         #         # every distinct object identified by the LIDAR
-#         #         for (lidar_angle_l, lidar_angle_r, dist) in lidar_input:
-#         #
-#         #             # print(str((lidar_angle_l, lidar_angle_r, dist)))
-#         #
-#         #             # convert box boundaries into angles, where 0 degrees is at the
-#         #             # middle of the image
-#         #             # note: this assumes the use of a fisheye lens, i.e. that angular
-#         #             # diameter is linearly related to distance in the image
-#         #             box_angle_l = (boxes[0][idx][1] - 0.5) * IM_ANGLE
-#         #             box_angle_r = (boxes[0][idx][3] - 0.5) * IM_ANGLE
-#         #             # print('L:' + str(box_angle_l))
-#         #             # print('R:' + str(box_angle_r))
-#         #
-#         #             # デモ用です
-#         #             if lidar_angle_l == -30:
-#         #                 print('Left 左側')
-#         #             if lidar_angle_l == -5:
-#         #                 print('Center 中央')
-#         #             if lidar_angle_l == 20:
-#         #                 print('Right 右側')
-#         #
-#         #             # if the closest point on an obstacle is less than MIN_DIST away
-#         #             if dist <= MIN_DIST:
-#         #                 # debug用のprint
-#         #                 # print('obstacle within range')
-#         #
-#         #                 # If the bounds of the LIDAR-detected object are entirely
-#         #                 # within the TensorFlow bounding box, then we will consider
-#         #                 # them to be representing the same object
-#         #                 # TODO: Figure out a more reliable way of deciding how
-#         #                 # to map LIDAR objects to bounding boxes from Tensorflow.
-#         #                 #
-#         #                 # LIDARが特定した物体の範囲が全部TensorFlowが特定したバウンディング
-#         #                 # ボックスの範囲以内であれば、LIDARが検出したものとTFが検出したものは、
-#         #                 # 同じ物体を示していると言えるでしょう
-#         #                 # TODO: もっとマシ・有効な手段を考えておこう。LIDARの物体とTFの
-#         #                 # 物体はどうすればマッピングできるのか？というのが問題です
-#         #                 if box_angle_l < lidar_angle_l and box_angle_r > lidar_angle_r:
-#         #                     # if the object is a human, send a signal to the GR-PEACH
-#         #                     # to wait, and stop looking at other
-#         #                     if int(category_index[int(classes[0][idx])].get('id'))==1:
-#         #                         print('Person detected. Waiting.')
-#         #                         print('人間発見。一旦待機します。')
-#         #
-#         #                         # TODO: transmit True to GR-PEACH
-#         #
-#         #                         # once there is a human within range, we can stop
-#         #                         # looping through all the LIDAR-detected objects
-#         #                         break
-#         #                     else:
-#         #                         print('Non-person obstacle detected. Rerouting.')
-#         #                         print('人間でない障害物発見。回避します。')
-#         #                 else:
-#         #                     print('No obstacle detected.')
-#         #                     print('物体が検出されていません。')
-#         #
-#         #         # continue looping if inside loop wasn't broken out of
-#         #         else:
-#         #             continue
-#         #
-#         #         # if break was called (i.e. if person was detected in range),
-#         #         # breaks out of the loop iterating through the TF-detected objects
-#         #         break
-#
-#         # find FPS, print into console
-#         t2 = cv2.getTickCount()
-#         time1 = (t2-t1)/freq
-#         frame_rate_calc = 1/time1
-#         print("FPS: " + str(frame_rate_calc))
-#
-#         rawCapture.truncate(0)
-#
+
 except KeyboardInterrupt:
     pass
 
